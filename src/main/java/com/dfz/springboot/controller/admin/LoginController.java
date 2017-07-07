46339bfd6f609a516a0af28f0b399b37c6e51fda
ding199309 <ding199309@163.com>
2017-07-07 14:15:59 +0800

    新增加入thymeleaf

---
+++ src/main/java/com/dfz/springboot/controller/admin/LoginController.java
@@ -0,0 +1,26 @@
+package com.dfz.springboot.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author dfz
+ * @version 1.0
+ * @E-mail: dfz@jkinvest.cn
+ * @date 创建时间： 10:32  2017/7/7
+ * @return
+ *
+ * 后台登录模块
+ */
+
+@Controller
+@RequestMapping("/admin")
+public class LoginController {
+
+
+
+    @RequestMapping("/loginInit")
+    public String  loginInit(){
+        return "admin/login/login";
+    }
+}
