10bad0c15ce697b8b3bd973a76bd1794c6d28aa4
fazhan.ding <fazhan.ding@yintech.cn>
2018-09-13 11:18:08 +0800

    修改配置文件展示方式

--- src/main/java/com/dfz/springboot/druid/DruidConfig.java
+++ src/main/java/com/dfz/springboot/druid/DruidConfig.java
@@ -25,61 +25,60 @@ import com.alibaba.druid.pool.DruidDataSource;
 
 
 @Configuration
-@ConfigurationProperties(prefix="share.datasource")
 public class DruidConfig {
 
-//	@Value("${share.datasource.url}")
+	@Value("${share.datasource.url}")
     private String url;
 
-//    @Value("${share.datasource.username}")
+    @Value("${share.datasource.username}")
     private String username;
 
-//    @Value("${share.datasource.password}")
+    @Value("${share.datasource.password}")
     private String password;
 
-//    @Value("${share.datasource.driverClassName}")
+    @Value("${share.datasource.driverClassName}")
     private String driverClassName;
     
-//    @Value("${share.datasource.initialSize}")
+    @Value("${share.datasource.initialSize}")
     private int initialSize;
 
-//    @Value("${share.datasource.minIdle}")
+    @Value("${share.datasource.minIdle}")
     private int minIdle;
 
-//    @Value("${share.datasource.maxActive}")
+    @Value("${share.datasource.maxActive}")
     private int maxActive;
 
-//    @Value("${share.datasource.maxWait}")
+    @Value("${share.datasource.maxWait}")
     private int maxWait;
 
-//    @Value("${share.datasource.timeBetweenEvictionRunsMillis}")
+    @Value("${share.datasource.timeBetweenEvictionRunsMillis}")
     private int timeBetweenEvictionRunsMillis;
 
-//    @Value("${share.datasource.minEvictableIdleTimeMillis}")
+    @Value("${share.datasource.minEvictableIdleTimeMillis}")
     private int minEvictableIdleTimeMillis;
 
-//    @Value("${share.datasource.validationQuery}")
+    @Value("${share.datasource.validationQuery}")
     private String validationQuery;
 
-//    @Value("${share.datasource.testWhileIdle}")
+    @Value("${share.datasource.testWhileIdle}")
     private boolean testWhileIdle;
 
-//    @Value("${share.datasource.testOnBorrow}")
+    @Value("${share.datasource.testOnBorrow}")
     private boolean testOnBorrow;
 
-//    @Value("${share.datasource.testOnReturn}")
+    @Value("${share.datasource.testOnReturn}")
     private boolean testOnReturn;
 
-//    @Value("${share.datasource.poolPreparedStatements}")
+    @Value("${share.datasource.poolPreparedStatements}")
     private boolean poolPreparedStatements;
 
-//    @Value("${share.datasource.maxPoolPreparedStatementPerConnectionSize}")
+    @Value("${share.datasource.maxPoolPreparedStatementPerConnectionSize}")
     private int maxPoolPreparedStatementPerConnectionSize;
 
-//    @Value("${share.datasource.filters}")
+    @Value("${share.datasource.filters}")
     private String filters;
 
-//    @Value("{share.datasource.connectionProperties}")
+    @Value("{share.datasource.connectionProperties}")
     private String connectionProperties;
 	
     @Bean(name = "masterDataSource")
@@ -105,148 +104,4 @@ public class DruidConfig {
         dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
         return dataSource;
     }
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public int getInitialSize() {
-		return initialSize;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getDriverClassName() {
-		return driverClassName;
-	}
-
-	public void setDriverClassName(String driverClassName) {
-		this.driverClassName = driverClassName;
-	}
-
-	public void setInitialSize(int initialSize) {
-		this.initialSize = initialSize;
-	}
-
-	public int getMinIdle() {
-		return minIdle;
-	}
-
-	public void setMinIdle(int minIdle) {
-		this.minIdle = minIdle;
-	}
-
-	public int getMaxActive() {
-		return maxActive;
-	}
-
-	public void setMaxActive(int maxActive) {
-		this.maxActive = maxActive;
-	}
-
-	public int getMaxWait() {
-		return maxWait;
-	}
-
-	public void setMaxWait(int maxWait) {
-		this.maxWait = maxWait;
-	}
-
-	public int getTimeBetweenEvictionRunsMillis() {
-		return timeBetweenEvictionRunsMillis;
-	}
-
-	public void setTimeBetweenEvictionRunsMillis(int timeBetweenEvictionRunsMillis) {
-		this.timeBetweenEvictionRunsMillis = timeBetweenEvictionRunsMillis;
-	}
-
-	public int getMinEvictableIdleTimeMillis() {
-		return minEvictableIdleTimeMillis;
-	}
-
-	public void setMinEvictableIdleTimeMillis(int minEvictableIdleTimeMillis) {
-		this.minEvictableIdleTimeMillis = minEvictableIdleTimeMillis;
-	}
-
-	public String getValidationQuery() {
-		return validationQuery;
-	}
-
-	public void setValidationQuery(String validationQuery) {
-		this.validationQuery = validationQuery;
-	}
-
-	public boolean isTestWhileIdle() {
-		return testWhileIdle;
-	}
-
-	public void setTestWhileIdle(boolean testWhileIdle) {
-		this.testWhileIdle = testWhileIdle;
-	}
-
-	public boolean isTestOnBorrow() {
-		return testOnBorrow;
-	}
-
-	public void setTestOnBorrow(boolean testOnBorrow) {
-		this.testOnBorrow = testOnBorrow;
-	}
-
-	public boolean isTestOnReturn() {
-		return testOnReturn;
-	}
-
-	public void setTestOnReturn(boolean testOnReturn) {
-		this.testOnReturn = testOnReturn;
-	}
-
-	public boolean isPoolPreparedStatements() {
-		return poolPreparedStatements;
-	}
-
-	public void setPoolPreparedStatements(boolean poolPreparedStatements) {
-		this.poolPreparedStatements = poolPreparedStatements;
-	}
-
-	public int getMaxPoolPreparedStatementPerConnectionSize() {
-		return maxPoolPreparedStatementPerConnectionSize;
-	}
-
-	public void setMaxPoolPreparedStatementPerConnectionSize(int maxPoolPreparedStatementPerConnectionSize) {
-		this.maxPoolPreparedStatementPerConnectionSize = maxPoolPreparedStatementPerConnectionSize;
-	}
-
-	public String getFilters() {
-		return filters;
-	}
-
-	public void setFilters(String filters) {
-		this.filters = filters;
-	}
-
-	public String getConnectionProperties() {
-		return connectionProperties;
-	}
-
-	public void setConnectionProperties(String connectionProperties) {
-		this.connectionProperties = connectionProperties;
-	}
-    
-    
 }
